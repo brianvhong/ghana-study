668d8c69f5623c6b71c3aa003245915f7b30ff69
Chenghao Zhu <zhuchcn@gmail.com>
2019-12-09 22:28:36 -0800

    add model 2 and model 3

--- R/explor/render.sh
+++ R/explor/render.sh
@@ -2,22 +2,22 @@
 POSITIONAL=()
 while [[ $# -gt 0 ]]
 do
-key="$1"
-
-case $key in
-    -h|--help)
-        echo "render.sh -h [-o|--output-dir] [input-file]"
-        exit
-        ;;
-    -o|--output-dir)
-        output_dir="$2"
-        shift 2
-        ;;
-    *)    
-        POSITIONAL+=("$1") # save it in an array for later
-        shift # past argument
-        ;;
-esac
+    key="$1"
+    
+    case $key in
+        -h|--help)
+            echo "render.sh -h [-o|--output-dir] [input-file]"
+            exit
+            ;;
+        -o|--output-dir)
+            output_dir="$2"
+            shift 2
+            ;;
+        *)    
+            POSITIONAL+=("$1") # save it in an array for later
+            shift # past argument
+            ;;
+    esac
 done
 set -- "${POSITIONAL[@]}" # restore positional parameters
 
